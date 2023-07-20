52d23be07b0f0d3daa27ed81d26fb5dbb6c723d3
MatthewPace98 <matthew.pobox@gmail.com>
2023-07-20 23:07:45 +0100

    Updated links to social media and wave animations

---
+++ static/js/waves.js
@@ -0,0 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    let mapElement = document.getElementById("map");
+    let mapText = mapElement.innerText;
+    let sea = "_.~\"~._";
+    let seaLength = sea.length;
+    let counter = 0;
+  
+    setInterval(() => {
+      let newMapText = "";
+      for (let i = 0; i < mapText.length; i++) {
+        if (mapText[i] === " ") {
+          let char = sea[(i + counter) % seaLength];
+          newMapText += `<span class="sea">${char}</span>`;
+        } else {
+          newMapText += mapText[i];
+        }
+      }
+      newMapText = newMapText.replace('UK', '<span class="country">UK</span>');
+        newMapText = newMapText.replace('Malta', '<span class="country">Malta</span>');
+  
+      mapElement.innerHTML = newMapText;
+      counter++;
+    }, 1200);
+  
+  });
