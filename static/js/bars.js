3f011345253d197577df31ac9338dfd5bb86183c
MatthewPace98 <matthew.pobox@gmail.com>
2023-10-01 23:09:15 +0100

    Added yellow and red competency bars

--- static/js/bars.js
+++ static/js/bars.js
@@ -7,16 +7,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
     
         function updateBar() {
             let currentWidth = element.textContent.replace(/ /g, '').length;
-    
+        
             if (currentWidth < maxBarWidth) {
                 currentWidth++;
+        
+                let redBars = currentWidth > 30 ? '|'.repeat(currentWidth - 30) : '';
+                let yellowBars = currentWidth > 25 ? '|'.repeat(Math.min(currentWidth, 30) - 25) : '';
+                let remainingBars = '|'.repeat(Math.min(currentWidth, 25));
+        
                 element.innerHTML =
-                    '<span class="c_low">' + '|'.repeat(currentWidth) + '</span>' +
+                    '<span class="c_low">' + remainingBars + '</span>' +
+                    '<span style="color: yellow;">' + yellowBars + '</span>' +
+                    '<span style="color: red;">' + redBars + '</span>' +
                     ' '.repeat(element.textContent.length - currentWidth);
             } else {
                 clearInterval(intervalId); // Stop updating when bar is full
             }
-        }
+        }             
     
         // Update the bar
         const intervalId = setInterval(updateBar, 70);
@@ -25,16 +32,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
         let observer = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 if(entry.isIntersecting) {
-                    animateBar('r', 28);
-                    animateBar('python', 28);
-                    animateBar('bash', 32);
+                    animateBar('r', 32);
+                    animateBar('python', 29);
+                    animateBar('bash', 31);
                     animateBar('web', 21);
-                    animateBar('latex', 19);
+                    animateBar('latex', 17);
                     animateBar('rna', 33);
-                    animateBar('wgs', 19);
-                    animateBar('crispr', 28); 
-                    animateBar('git', 25); 
-                    animateBar('ai', 19); 
+                    animateBar('wgs', 20);
+                    animateBar('crispr', 29); 
+                    animateBar('git', 27); 
+                    animateBar('ai', 22); 
     
                     observer.unobserve(entry.target); // Stop observing once the animation is started
                 }
