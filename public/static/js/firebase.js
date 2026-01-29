1cd2915cfc0c7e03e995caf608d1f2e02c112e10
Matthew Pace <matthew.pobox@gmail.com>
2026-01-29 15:57:22 +0000

    Relocated files so that they follow a more typical file structure

---
+++ public/static/js/firebase.js
@@ -0,0 +1,18 @@
+// Initializes Firebase for hosting
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD84NJ1oN0pJ2NW7gGBSEofTlHYCwCvo84",
+  authDomain: "maltiomics.firebaseapp.com",
+  projectId: "maltiomics",
+  storageBucket: "maltiomics.appspot.com",
+  messagingSenderId: "1039312535834",
+  appId: "1:1039312535834:web:781d07817b94c5e985aca3",
+  measurementId: "G-TQWL80D1KT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
